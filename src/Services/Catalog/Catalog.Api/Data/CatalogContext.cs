7ddde863b5ef23b47c09eb23f8608218e6766e4d
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-11-12 21:55:01 +0330

    using database and datacontext and connection strings and entity with mongoDB.

---
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var databaseName = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = databaseName.GetCollection<Product>("DatabaseSettings:CollectionName");
+
+            CatalogContextSeed.SeedData(Products);
+
+        }
+
+    }
+}
