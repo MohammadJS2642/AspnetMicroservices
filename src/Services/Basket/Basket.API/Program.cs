00cc72575bfb3eb6afac066df65ecca60b4b0fc9
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-11-15 20:45:19 +0330

    Redis DI Added.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -7,6 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Redis Connection
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
