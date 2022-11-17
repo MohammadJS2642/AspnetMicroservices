91d59a688b87e9da89ca01530033b57d17fbff18
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-11-17 11:26:51 +0330

    deleted not needed files.

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -21,7 +21,7 @@ app.MigraieDatabase<Program>();
 // Configure the HTTP request pipeline.
 
 // Map GRPC Service added with Grpc
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
