e091fc33582aa385ab81071e0bc4146bc7f67fb8
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-11-18 23:23:51 +0330

    created MediatR command and command handler also added mapper for that and validator for commands in CQRS with fluentValidator.

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty()
+                .WithMessage("{UserName} is Required")
+                .NotNull()
+                .MaximumLength(50)
+                .WithMessage("{UserName} must not be exceed 50 characters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty()
+                .WithMessage("{EmailAddress} is Required");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty()
+                .WithMessage("{TotalPrice} is Required")
+                .GreaterThan(0)
+                .WithMessage("{TotalPrice} should be greater than zero.");
+        }
+    }
+}
