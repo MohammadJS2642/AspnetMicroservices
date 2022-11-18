670226faad2c91c9e0f81f72808d9e5973437fae
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-11-18 19:13:04 +0330

    added needed .

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Contracts.Infrastructure
+{
+
+    using Ordering.Application.Model;
+
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
