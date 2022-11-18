337471e4d47917eb8d18a1595353edec5f8871e9
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-11-18 18:44:49 +0330

    Added IAsyncRepository and IOrderRepository Persistance layer.

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Contracts.Persistence
+{
+    using Ordering.Domain.Entities;
+
+    public interface IOrderRepositorys : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUsername(string userName);
+    }
+}
