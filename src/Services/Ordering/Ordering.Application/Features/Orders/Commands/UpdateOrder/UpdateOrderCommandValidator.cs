95ae387498b3a72de1ab4726cc2f5d8a6a85fd8c
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-11-19 22:09:10 +0330

    created Update Command for CQRS with MediatR .

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty()
+                .WithMessage("{UserName} is Required")
+                .NotNull()
+                .MaximumLength(50)
+                .WithMessage("{UserName} must not be exceed 50 characters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty()
+                .WithMessage("{EmailAddress} is Required");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty()
+                .WithMessage("{TotalPrice} is Required")
+                .GreaterThan(0)
+                .WithMessage("{TotalPrice} should be greater than zero.");
+
+        }
+
+    }
+}
