b5c7c6c2901c4cefa5e7990cea443b16a1a7a4f1
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-11-24 20:19:22 +0330

    added Persistence architecture.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public static class OrderContextSeed
+    {
+
+        public static async Task SeedAsync(OrderContext orderContext)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order(){
+                    UserName="MJS",
+                    FirstName="Mohammad Javad",
+                    LastName="Sardehghan",
+                    EmailAddress="mohammadjs2642@gmail.com",
+                    AddressLine="Tehran",
+                    Country="Iran",
+                    TotalPrice=1000
+                }
+            };
+        }
+
+    }
+}
